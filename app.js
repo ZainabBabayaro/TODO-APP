a364c21c11c0fe79ba7ee16620e8bc6ef326b0b4
Zainab Babayaro <babayarozainab@gmail.com>
2022-12-15 09:07:08 +0100

    activate green button

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ function myfunc() {
     todos.innerHTML += `<li class="task">
         <p>${inputField.value}</p>
         <div>
-            <button class="button-1">✓</button>
+            <button class="button-1" onclick="completeTodo(event)">✓</button>
             <button class="button-2" onclick="deleteTodo(event)">𝑥</button>
         </div>
     </li>`
 
     inputField.value = ''
     totalTodos()
+    
 }
 
 function deleteTodo (event) {
@@ -34,9 +35,36 @@ function deleteTodo (event) {
     totalTodos()
 }
 
+
 function totalTodos() {
     const todos = document.querySelectorAll(".task");
     const todoCount = document.querySelector('.todo-count')
 
     todoCount.textContent = todos.length
+}
+
+
+
+
+function completeTodo (event) {
+
+     const target = event.target.parentNode.previousElementSibling.textContent
+     const listOfTodos = document.querySelectorAll('.task')
+
+      listOfTodos.forEach(element => {
+          if (element.children[0].textContent == target) {
+              element.style.color = "lightgrey";
+          }
+      });
+
+        totalCompleted()
+   
+ }
+
+
+
+function totalCompleted(){
+     const completed = document.querySelector(".total-completed");
+   
+     completed.textContent = completeTodo.length;
 }
